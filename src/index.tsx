2d34a3d715745462407e36af3035900fe54dad21
Yamil Rivera <yamil.rivera@wizeline.com>
2020-04-28 20:34:21 -0400

    Switching to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
 // TODO: Not working...
 require('dotenv').config();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <div>
-        <Route exact path='/' component={App} />
-      </div>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router>
+            <div>
+                <Route exact path="/" component={App} />
+            </div>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
