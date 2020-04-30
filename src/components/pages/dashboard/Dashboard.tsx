2dd3f2c54bd793e46e890b281b746e7cb745a287
Alexis Munoz <alexismun53@gmail.com>
2020-04-30 15:35:39 -0500

    Add header content

--- src/components/pages/dashboard/Dashboard.tsx
+++ src/components/pages/dashboard/Dashboard.tsx
@@ -4,13 +4,14 @@ import 'antd/dist/antd.css';
 import NotAuthUserHeader from '../common/header/NotAuthUserHeader';
 import BasicFooter from '../common/footer/Footer';
 import DashboardContent from './DashboardContent';
+import './Dashboard.css'
 
 const Dashboard = () => {
     return (
         <div>
             <Layout>
-                <NotAuthUserHeader />
-                <DashboardContent />
+                <NotAuthUserHeader/>
+                <DashboardContent/>
                 <BasicFooter />
             </Layout>
         </div>
